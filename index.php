5c42bc6e6f1cd4e5b295c055eec76c4fce0832d0
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-04 18:43:30 +0300

    converting file to php

--- index.php
+++ index.php
@@ -51,7 +51,8 @@ session_start();
     <h2 style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:rgb(3, 50, 205);"><u>WELCOME TO BETTER HELP!</u></h2>
     
     <footer style="text-align:end; color:aliceblue; background-color: black; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">All rights reserved</footer>
-    <?php require_once()
+    <?php require_once("form.php"); ?>
+    
     
    
     
