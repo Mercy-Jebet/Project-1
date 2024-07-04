b237cbfdb82b79b858e2eda3c4d58788a5fce286
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-04 22:33:06 +0300

    converting file to php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php
 session_start();
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
