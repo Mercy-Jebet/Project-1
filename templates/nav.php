412e18040036d3848ab69119237dc47b57e70a94
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-05 11:54:17 +0300

    adding the navigation file

---
+++ templates/nav.php
@@ -0,0 +1,5 @@
+<div class = "toplink">
+        <a href = "index.html">HOME</a>
+        <a href="contact_us.html">CONTACT</a>
+        <a href="about.html">ABOUT</a>
+        <a href ="form.html">LOGIN</a>
