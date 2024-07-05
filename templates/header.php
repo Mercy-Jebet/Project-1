58238ba414d2de04816d870e24c561da652689ca
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-05 11:35:26 +0300

    adding the header

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv ="Content-Type" content= "text/html;charset=UTF-8">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=, initial-scale=1.0">
         <title>Document</title>
-        <link rel="stylesheet" href="css/style.css">
 </head>
