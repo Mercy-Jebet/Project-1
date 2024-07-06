11fbbea6f578aab41b409d8a8bcb400526a43c7a
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-06 10:23:15 +0300

    converting file to php

--- index.php
+++ index.php
@@ -27,19 +27,11 @@
 
 <body>
    <header> <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;" color="#0b7f97; " align="center">BETTER HELP</h1>
-    <div class=""Header>
-        
-        <h2 id ="menu" align ="middle" style="font-family:'Times New Roman', Times, serif; color: darkgrey; ">
-        <a href = "index.html">HOME</a>
-        <a href="contact_us.html">CONTACT</a>
-        <a href="about.html">ABOUT</a>
-        <a href ="form.html">LOGIN</a>
-        </h2>
-    </div></header>
-    <img align="left" src="group.jpg" style="width:500px; height:400px">
-    <h2 style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:rgb(3, 50, 205);"><u>WELCOME TO BETTER HELP!</u></h2>
+    <div class="content">
+        <img align="left" src="group.jpg" style="width:500px; height:400px">
+        <h2 style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:rgb(3, 50, 205);"><u>WELCOME TO BETTER HELP!</u></h2>
     
-    <footer style="text-align:end; color:aliceblue; background-color: black; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">All rights reserved</footer>
+    
     
     
    
