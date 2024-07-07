86528f6c517e190309fa6838033de177ccb8f130
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-07 23:50:08 +0300

    editing the header

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE php>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=, initial-scale=1.0">
+        <meta http-equiv="Content-Type" content= "text/html:charset=UTF-8">
+        
         <title>Document</title>
 </head>
