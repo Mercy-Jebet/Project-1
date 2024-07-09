d3a35d67625b359a6ae876543175510461cb363c
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-09 09:26:38 +0300

    editing the about.php file

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
-<?php include_once ("templates/heading.php");?>
+<?php include_once ("templates/header.php");?>
 <?php include_once ("templates/nav.php")?>
 <?php include_once ("templates/footer.php")?>
 
