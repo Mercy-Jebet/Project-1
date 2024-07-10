f440b3c688342fc71785421e147ba36c3aec0835
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-10 22:27:20 +0300

    creating the database

--- SQL/db.sql
+++ SQL/db.sql
@@ -10,5 +10,7 @@ CREATE TABLE IF NOT EXISTS sign_in(
     fullname varchar(50) NOT NULL DEFAULT '',
     username varchar(50) NOT NULL DEFAULT '',
     pass.word varchar(50) NOT NULL DEFAULT '',
-    
+    day.of.birth date NOT NULL DEFAULT 0;
+    gender varchar(50) NOT NULL DEFAULT '',
+    PRIMARY KEY (userId)
 );
