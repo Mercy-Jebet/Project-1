a7e7e6d5a0a31ad18cbf72032ae2db06543449f6
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-10 22:00:53 +0300

    creating the database

--- SQL/db.sql
+++ SQL/db.sql
@@ -2,3 +2,13 @@
 
 DROP DATABASE IF EXISTS Better Help;
 CREATE DATABASE IF NOT EXISTS Better Help;
+
+--create table
+
+DROP TABLE IF EXISTS sign_in;
+CREATE TABLE IF NOT EXISTS sign_in(
+    fullname varchar(50) NOT NULL DEFAULT '',
+    username varchar(50) NOT NULL DEFAULT '',
+    pass.word varchar(50) NOT NULL DEFAULT '',
+    
+);
