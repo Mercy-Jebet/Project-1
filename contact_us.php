b317e095428acc788b825a61c9cfc4329f3ce23a
Mercy-Jebet <mercy.wendot@strathmore.edu>
2024-07-11 11:17:00 +0300

    modifying the contact us page

--- contact_us.php
+++ contact_us.php
@@ -1,34 +1,21 @@
 <?php include_once ("templates/header.php");?>
 <?php include_once ("templates/nav.php")?>
 <?php include_once ("templates/footer.php")?>
-        div{
-            margin:auto;
-            background-color:rgb(220, 211, 211);
-            text-align: center;
-        }
-        p{
-            margin: 1;
-        }
-        header{
-            background-color:rgb(0, 0, 0);
-            padding:0%;
-            text-align: center;
-            font-size:x-large;
-            color:aliceblue;
-        }
-        footer {
-            background-color: #777;
-            padding: 10px;
-            text-align: center;
-            color: white;
-        }</style>
+        
          
 </head>
 <body>
     <header> <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;" color="#0b7f97; " align="center">BETTER HELP</h1>
-    <h2>
-    
-    </h2></header>
+    </header>
+    <h2><b>Contact us</b></h2>
+    <p>We're here to help!  Whether you have questions, concerns, or feedback about our services, our friendly support team is happy to assist you.</p>
+    <p><b>Here are some ways to reach us:</b></p>
+    <p><b>Contact Form:</b> Fill out the quick and easy form below to send a message directly to our Customer Success Team. Be sure to let us know if you have a general question, billing inquiry, or other specific request.</p>
+    <p><b>FAQ:</b>Visit our Frequently Asked Questions page for answers to many common questions about BetterHelp services, subscriptions, and billing.</p>
+    <p><b>Phone:</b>For immediate assistance, you can also call our support line at 555-030.</p>
+
+
+
 </body>
 <footer>All rights reserved </footer>
 <?php include_once ("templates/header.php");?>
